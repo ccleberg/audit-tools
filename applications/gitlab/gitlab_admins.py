4a7fbd1cc889aaaf4acd93018a061c334fba697b
Christian Cleberg <hello@cleberg.net>
2024-12-28 12:16:48 -0600

    pylint fixes

--- applications/gitlab/gitlab_admins.py
+++ applications/gitlab/gitlab_admins.py
@@ -13,11 +13,11 @@ URL = f"{BASE_URL}/groups/{GROUP_ID}/members"
 HEADERS = {"PRIVATE-TOKEN": PRIVATE_TOKEN}
 
 if __name__ == '__main__':
-	# Get group members
-	response = requests.get(URL, headers=HEADERS, timeout=TIMEOUT)
-	if response.status_code == 200:
-		members = response.json()
-		for member in members:
-			print(f"Username: {member['username']}, Access Level: {member['access_level']}")
-	else:
-		print(f"Failed to fetch group members: {response.status_code}, {response.text}")
+    # Get group members
+    response = requests.get(URL, headers=HEADERS, timeout=TIMEOUT)
+    if response.status_code == 200:
+        members = response.json()
+        for member in members:
+            print(f"Username: {member['username']}, Access Level: {member['access_level']}")
+    else:
+        print(f"Failed to fetch group members: {response.status_code}, {response.text}")
