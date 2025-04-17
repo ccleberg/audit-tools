5a8c13dc41831282fc348540a9776934ec9cf6a1
Christian Cleberg <hello@cleberg.net>
2025-04-17 13:46:43 -0500

    move excess mysql password query to new script

--- databases/passwords/mysql/passwords.sql
+++ databases/passwords/mysql/passwords.sql
@@ -4,6 +4,10 @@
 -- Authentication methods only
 SELECT user, host, plugin FROM mysql.user;
 
+-- Default password configuration only
+SHOW GLOBAL VARIABLES LIKE 'validate_password%';
+SHOW VARIABLES LIKE 'validate_password%';
+
 -- Authentication methods and MySQL password configurations
 sql
 SELECT 
